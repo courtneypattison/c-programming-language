375fcf233f8018403eda62c0050b6e29a90fae89
mlisccourtney <mliscourtney@gmail.com>
2016-06-14 09:56:45 -0400

    invert now only inverts n bits

--- ch2/2-07-invert.c
+++ ch2/2-07-invert.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
-unsigned getbits(unsigned x, int p, int n)
+unsigned getInvertedbits(unsigned x, int p, int n)
 {
-    return (~x >> (p + 1 - n)) & ~(~0 << n);
+    return ((~x >> (p + 1 - n)) & ~(~0 << n)) << (p + 1 - n);
 }
 
 unsigned invert(unsigned x, int p, int n)
 {
-    x = getbits(x, p, n);
+    unsigned inverted, mask, x_mask;
 
-    x = x << (p + 1 - n);
-    
+	mask = 0;
+    mask = (~mask << n) << (p + 1 - n) | ~(~0 << (p + 1 - n));
 
-    return x;
+	x_mask = x & mask;
+
+    inverted = getInvertedbits(x, p, n);
+	inverted |= x_mask;
+
+    return inverted;
 }
 
 int main()
